c1dc339236927e9f76aa50c296ff30b72ce23a93
BetjaderOD <20213tn032@utez.edu.mx>
2022-12-04 20:56:27 -0600

    cart

--- CapyGamesA/src/app/app.module.ts
+++ CapyGamesA/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NavigationComponent} from "./shared/navigation/navigation..component";
+import {CartModule} from "./modules/Customer/cart/cart.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,NavigationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    CartModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
