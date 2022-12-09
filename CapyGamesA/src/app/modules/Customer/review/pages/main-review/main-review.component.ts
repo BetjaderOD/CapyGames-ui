d07939687f5c71d38fcad3a417f04cfc336d7eec
Abraham <20213tn037@utez.edu.mx>
2022-12-08 21:38:16 -0600

    Review aun trabajando

--- CapyGamesA/src/app/modules/Customer/review/pages/main-review/main-review.component.ts
+++ CapyGamesA/src/app/modules/Customer/review/pages/main-review/main-review.component.ts
@@ -7,37 +7,37 @@ import {LiveAnnouncer} from "@angular/cdk/a11y";
 import {MatDialog} from "@angular/material/dialog";
 import {ReviewService} from "../../services/review.service";
 
-// @Component({
-//   selector: 'app-main-reveiw',
-//   templateUrl: './main-review.component.html',
-//   styleUrls: ['./main-review.component.css'],
-// })
-// export class MainReviewComponent implements OnInit{
-//   displayedColumns: string[] = [
-//   '#',
-//   'date',
-//   'title',
-//   'description',
-//   'rating',
-// ];
-//   @ViewChild(MatPaginator) paginator!: MatPaginator;
-//   @ViewChild(MatSort) sort!: MatSort;
-//   review!: MatTableDataSource<Review>;
-// constructor(private readonly reviewService: Review,
-//   private _liveAnnouncer: LiveAnnouncer,
-//   public dialog: MatDialog,) {}
-//
-//   ngOnInit() {
-//     this.getAllReview();
-//   }
-//
-//   getAllReview(){
-//       this.reviewService.findAll().subscribe((res)=>{
-//       this.review = new MatTableDataSource<Review>(res);
-//       this.review.paginator = this.paginator;
-//       this.review.sort = this.sort;
-//     });
-//   }
-//
+@Component({
+  selector: 'app-main-reveiw',
+  templateUrl: './main-review.component.html',
+  styleUrls: ['./main-review.component.css'],
+})
+export class MainReviewComponent implements OnInit{
+  displayedColumns: string[] = [
+  '#',
+  'date',
+  'title',
+  'description',
+  'rating',
+];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  review!: MatTableDataSource<Review>;
+constructor(private readonly reviewService: Review,
+  private _liveAnnouncer: LiveAnnouncer,
+  public dialog: MatDialog,) {}
+
+  ngOnInit() {
+    this.getAllReview();
+  }
+
+  getAllReview(){
+      this.reviewService.findAll().subscribe((res)=>{
+      this.review = new MatTableDataSource<Review>(res);
+      this.review.paginator = this.paginator;
+      this.review.sort = this.sort;
+    });
+  }
+
 
 
