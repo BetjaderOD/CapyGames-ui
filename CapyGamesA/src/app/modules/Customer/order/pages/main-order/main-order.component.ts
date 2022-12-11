c6ed9ab0a874ec1301bb930456e96ef9e601d672
Miguel <20213tn055@utez.edu.mx>
2022-12-10 20:10:04 -0600

    error en el component.ts

--- CapyGamesA/src/app/modules/Customer/order/pages/main-order/main-order.component.ts
+++ CapyGamesA/src/app/modules/Customer/order/pages/main-order/main-order.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
 
+class Order {
+  id: number;
+  name: string;
+  price: number;
+  quantity: number;
+  totalPrice: number;
+}
+
 @Component({
   selector: 'app-order',
-  templateUrl: './main-order.component.html', 
+  templateUrl: './main-order.component.html',
   styleUrls: ['./main-order.component.css'],
 })
 
 export class OrderComponent {
+    path: 'order/:id';
+  order: any;
+  
     constructor() {}
+  
+    viewOrder(
+      order: Order,
+    ) {
+      this.router.navigate([`/order/${order.id}`]);
+    }
   }
