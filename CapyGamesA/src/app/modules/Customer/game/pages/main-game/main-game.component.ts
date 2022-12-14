0eee110e87c4e3df98aec5bbb5c3317126119347
BetjaderOD <20213tn032@utez.edu.mx>
2022-12-13 19:08:28 -0600

    rama

--- CapyGamesA/src/app/modules/Customer/game/pages/main-game/main-game.component.ts
+++ CapyGamesA/src/app/modules/Customer/game/pages/main-game/main-game.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { GameService } from '../../services/game.service';
 import { Game } from '../../types/game';
 import { Router } from '@angular/router';
+import { CartService } from '../../../cart/service/cart.service';
 
 @Component({
   selector: 'app-main-game',
@@ -18,7 +19,8 @@ export class MainGameComponent implements OnInit {
 
   constructor(
     private readonly _gameService: GameService,
-    private router: Router
+    private router: Router,
+    private _cartService: CartService
   ) {}
 
   ngOnInit(): void {
